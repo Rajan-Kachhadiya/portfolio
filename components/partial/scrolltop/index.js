0afe93c6a179743450387bca991e1d4a1e2b0b16
Rajan Kachhadiya <rajan.kachhadia@gmail.com>
2022-12-17 16:23:26 +0530

    Desgin Changes

--- components/partial/scrolltop/index.js
+++ components/partial/scrolltop/index.js
@@ -6,15 +6,13 @@ export default function ScrollTop() {
 
     return (
         <div className="scroll-top-box">
-            <ScrollToTop smooth component={<ArrowUpIcon />} />
+            <ScrollToTop smooth component={<ArrowUpIcon />} className="!rounded-full !right-30 !bottom-30 inline-flex items-center justify-center !bg-white/70 !w-[50px] !h-[50px] transition-all duration-[0.4s] relative before:transition-all before:duration-[0.4s] before:absolute before:content-[''] before:left-0 before:top-0 before:w-full before:h-full before:bg-gradient-box-hover before:z-[-1] before:rounded-full before:opacity-0 hover:before:opacity-100"/>
             <style>{`
-                .scroll-top-box button{
-                    border-radius: 50%;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    width: 50px;
-                    height: 50px;
+                .scroll-top-box button svg path{
+                    transition: 0.2s all ease-in-out;
+                }
+                .scroll-top-box button:hover svg path{
+                    fill: white;
                 }
             `}</style>
         </div>
